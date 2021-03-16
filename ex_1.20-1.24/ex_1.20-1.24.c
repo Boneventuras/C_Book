1d29a23c820a12b822e3cc09ee501483e1b91a3c
Bomb Letas <arturas.venckus1986@gmail.com>
2021-03-16 23:13:39 +0200

    Excersise 1.20 to 1.24 push

---
+++ ex_1.20-1.24/ex_1.20-1.24.c
@@ -0,0 +1,51 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "lib_ex_1.20-1.24.h"
+#include "lib_ex_1.20-1.24.c"
+
+int main()
+{
+	// Pasidometi ASCII escape chars for color text
+	char c, *cc;
+	
+	while(1)
+	{
+		//test();
+		puts("Availible functions for strings:");
+		puts("\t1 - detab (replaces tabs with spaces)");
+		puts("\t2 - entb (replaces multiple spaces with tabs)");
+		puts("\033[35;106mSelect one function by entering its number:\033[m");
+		while((c = getchar()) == '\n' && c == '\0')
+			;
+		if(isdigit(c))
+		{
+			switch(c)
+			{
+				case '1':
+				detab();
+				break;
+				
+				case '2':
+				puts("\n\033[31;106mentab\033[m\n");
+				test();
+				break;
+				
+				case '0':
+				puts("Exit?\ny - for yes\t\tenter any other vlues to continue");
+				while((c = getchar()) == '\n' || c == '\0')
+					;
+				if(c == 'y' || c == 'Y')
+					return 0;
+			}
+			if(c != '\n' && c != '\0')
+				getchar();
+		}
+		else
+		{
+			puts("\n\033[31;106mNot valid entry, please select again\033[m\n");	
+			if((c = getchar()) != '\n' && c != '\0')
+				putchar(c);
+		}
+	}
+}
---
+++ ex_1.20-1.24/lib_ex_1.20-1.24.c
@@ -0,0 +1,81 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+#include "lib_ex_1.20-1.24.h"
+
+/*
+Exercise 1-20. Write a program detab that replaces
+tabs in the input with the proper number of blanks
+to space to the next tab stop. Assume a fixed set of
+tab stops, say every n columns. Should n be a
+variable or a symbolic parameter?
+*/
+
+int test ()
+{
+	puts("Running test");
+	return 0;
+}
+
+int detab (void)
+{
+	short int tbStop;		// tabStop length
+	short int i = 0;			// index for iterations
+	char str[MAXSTRLEN];	// Array for user input string
+	
+	puts("\n\033[31;106mRunning detab\033[m\n");
+	fputs("Enter single digit number (0 - 9) to set tabstop: ",stdout);
+	while((tbStop = getchar()) == '\n' && tbStop == '\0' && !isdigit(tbStop) && (i < 5))
+		tbStop == '\n' || tbStop == '\0' ? i++ :1;
+	while(getchar() == '\n')
+		;
+	if(i == 5)
+	{
+		puts("5 incorrect inputs, returning to main\n");
+		return 0;
+	}	
+	printf("tabstop set to %d\n", tbStop);
+	puts("Enter string to replace tabs with spaces:");
+	for(i = 0; (str[i] = getchar()) != '\0' && str[i] != '\n' && i < (MAXSTRLEN - 1); i++)
+		puts("1");
+	str[i] = '\0';
+	
+	puts(str);
+	printf("num of chars %d.\n", i);
+	
+	return 0;
+}
+
+/*
+Exercise 1-21. Write a program entab that replaces
+strings of blanks by the minimum number of tabs and
+blanks to achieve the same spacing. Use the same tab
+stops as for detab. When either a tab or a single
+blank would suffice to reach a tab stop, which
+should be given preference?
+*/
+
+/*
+Exercise 1-22. Write a program to ``fold'' long
+input lines into two or more shorter lines after the
+last non-blank character that occurs before the n-th
+column of input. Make sure your program does
+something intelligent with very long lines, and if
+there are no blanks or tabs before the specified
+column.
+*/
+
+/*
+Exercise 1-23. Write a program to remove all
+comments from a C program. Don't forget to handle
+quoted strings and character constants properly. C
+comments don't nest.
+*/
+
+/*
+Exercise 1-24. Write a program to check a C program
+for rudimentary syntax errors like unmatched
+parentheses, brackets and braces. Don't forget about
+quotes, both single and double, escape sequences,
+and comments. (This program is hard if you do it in
+full generality.)
+*/
