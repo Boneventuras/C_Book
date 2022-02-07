2f25d60e3ae18b2fdabd8be58246706a15108d2f
Boneventuras <arturas.venckus1986@gmail.com>
2022-02-08 00:08:16 +0200

    Added shell script for automated testing, need to add comments how to use it

---
+++ LCTHW/ex17/testscript.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+echo -e "\nRunning test for $1\n"
+
+check_status(){
+if [ "$2" != "0" ]
+	then
+		echo -e "\n$1 exit status is $2\n"
+		exit
+fi
+}
+
+# create db
+./$1 x.txt c 4 7
+status=$?
+check_status $1 $?
+
+./$1 x.txt s 0 Tom Tmail 45 23 Rukla
+status=$?
+check_status $1 $?
+
+./$1 x.txt s 1 Dick Stick 10 1 Skuodas
+status=$?
+check_status $1 $?
+
+./$1 x.txt s 2 Will Robot
+status=$?
+check_status $1 $?
+
+./$1 x.txt s 3 Steve Jhon 8
+status=$?
+check_status $1 $?
+
+./$1 x.txt l
+status=$?
+check_status $1 $?
+
+echo -e "\n$1 exit status is $status\n"
+
+xxd -c 8 x.txt
+
+# save entries to db
+
